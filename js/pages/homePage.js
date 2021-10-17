ddd2130c2d854eed47e66ee30134d892894045a5
unknown <videnovic99@hotmail.com>
2021-10-17 15:16:45 +0200

    add-project-files

---
+++ js/pages/homePage.js
@@ -0,0 +1,191 @@
+function homePage(){
+    const homePageElements = `<!--BANNER START-->
+    <section id="banner" class="banner">
+        <div class="banner-content-left">
+            <h1>NIKE AIR<br>MAX COLLECTION</h1>
+            <p>Inspiring the world’s athletes, Nike delivers inovative products, experiences and services.</p>
+            <a href="https://www.nike.com/w/mens-air-max-shoes-a6d8hznik1zy7ok" target="_blank">EXPLORE</a>
+        </div>
+        <div class="banner-content-right">
+            <div class="back-element">
+                <div class="top-content">
+                    <img src="./images/global/logoNoTextWhite.png" alt="logo">
+                    <h2>JUST DO IT</h2>
+                </div>
+                <div class="bottom-content">
+                    <h2>NIKE SHOES</h2>
+                </div>
+            </div>
+            <img class="shoe shoe-color" src="./images/global/redNikeShoe.png" alt="red-shoe">
+        </div>
+        <div class="colors">
+            <p>Colors:</p>
+            <div class="circle-wraps">
+                <div class="circle red active">
+                    <div class="circle-border"></div>
+                </div>
+                <div class="circle blue">
+                    <div class="circle-border"></div>
+                </div>
+                <div class="circle green">
+                    <div class="circle-border"></div>
+                </div>
+            </div>
+        </div>
+        <div class="social-icons-link-page-btn">
+            <div class="social-icons">
+                <a href="#" class="icon">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" class="icon">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="#" class="icon">
+                    <i class="fab fa-twitter"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+    <!--BANNER END-->
+    <!--ABOUT US SECTION START-->
+    <section id="about" class="about">
+        <div class="about-logo">
+            <img src="./images/global/logoTextBlack.png" alt="logo">
+        </div>
+        <div class="about-info">
+            <h2>ABOUT</h2>
+            <p>Nike Air Max is a line of shoes produced by Nike, Inc., with the first model released in 1987. Air Max shoes are identified by their midsoles incorporating flexible urethane pouches filled with pressurized gas, visible from the exterior of the shoe and intended to provide cushioning underfoot. Air Max was conceptualized by Tinker Hatfield, who initially worked for Nike designing stores.</p>
+            <a href="about.html">
+                SEE MORE
+            </a>
+        </div>
+    </section>
+    <!--ABOUT US SECTION END-->
+    <!--QUOTE SECTION START-->
+    <section id="quote-section" class="quote-section">
+        <div class="quote-section-quote">
+            <cite>TOMORROW<br>YOU WILL WISH<br>YOU HAD<br>STARTED TODAY</cite>
+        </div>
+        <div class="quote-section-logo">
+            <img src="./images/global/logoTextWhite.png" alt="logo">
+        </div>
+    </section>
+    <!--QUOTE SECTION END-->
+    <!--RELATED PRODUCTS START-->
+    <section id="related" class="related">
+        <h2 class="section-head">RELATED PRODUCTS</h2>
+        <ul class="related-carousel">
+            <li class="item">
+                <div class="item-info">
+                    <img src="./images/homePage/basketballShoe.jpg" alt="basketball-shoes">
+                    <h3>Basketball Shoes</h3>
+                </div>
+                <a href="https://www.nike.com/w/mens-basketball-shoes-3glsmznik1zy7ok" target="_blank">EXPLORE</a>
+            </li>
+            <li class="item">
+                <div class="item-info">
+                    <img src="./images/homePage/soccerShoe.webp" alt="soccer-shoes">
+                    <h3>Soccer Shoes</h3>
+                </div>
+                <a href="https://www.nike.com/w/mens-soccer-shoes-1gdj0znik1zy7ok" target="_blank">EXPLORE</a>
+            </li>
+            <li class="item">
+                <div class="item-info">
+                    <img src="./images/homePage/lifestyleShoe.webp" alt="lifestyle-shoes">
+                    <h3>Lifestyle Shoes</h3>
+                </div>
+                <a href="https://www.nike.com/w/mens-lifestyle-shoes-13jrmznik1zy7ok" target="_blank">EXPLORE</a>
+            </li>
+            <li class="item">
+                <div class="item-info">
+                    <img src="./images/homePage/runningShoe.jpg" alt="running-shoes">
+                    <h3>Running Shoes</h3>
+                </div>
+                <a href="https://www.nike.com/w/mens-running-shoes-37v7jznik1zy7ok" target="_blank">EXPLORE</a>
+            </li>
+            <li class="item">
+                <div class="item-info">
+                    <img src="./images/homePage/nikePro.webp" alt="nike-pro">
+                    <h3>Nike Pro</h3>
+                </div>
+                <a href="https://www.nike.com/w/mens-nike-pro-3cqxfznik1" target="_blank">EXPLORE</a>
+            </li>
+            <li class="item">
+                <div class="item-info">
+                    <img src="./images/homePage/hoodies.webp" alt="hoodies">
+                    <h3>Hoodies & Sweatshirts</h3>
+                </div>
+                <a href="https://www.nike.com/w/mens-hoodies-pullovers-6riveznik1" target="_blank">EXPLORE</a>
+            </li>
+            <li class="item">
+                <div class="item-info">
+                    <img src="./images/homePage/polos.webp" alt="polos">
+                    <h3>Polos</h3>
+                </div>
+                <a href="https://www.nike.com/w/mens-polos-9uaqmznik1" target="_blank">EXPLORE</a>
+            </li>
+            <li class="item">
+                <div class="item-info">
+                    <img src="./images/homePage/shorts.webp" alt="shorts">
+                    <h3>Shorts</h3>
+                </div>
+                <a href="https://www.nike.com/w/mens-shorts-38fphznik1" target="_blank">EXPLORE</a>
+            </li>
+            <li class="item">
+                <div class="item-info">
+                    <img src="./images/homePage/golfShoe.webp" alt="golf-shoes">
+                    <h3>Golf Shoes</h3>
+                </div>
+                <a href="https://www.nike.com/w/mens-golf-shoes-23q9wznik1zy7ok" target="_blank">EXPLORE</a>
+            </li>
+        </ul>
+    </section>
+    <!--RELATED PRODUCTS END-->
+    <!--CUSTOMER REVIEWS START-->
+    <section id="customers-reviews" class="customers-reviews">
+        <h2 class="section-head">CUSTOMER REVIEWS</h2>
+        <ul class="reviews-wrap">
+            <li class="review">
+                <div class="user-img">
+                    <img src="./images/reviews/martinNichols.jpg" alt="user-img">
+                </div>
+                <div class="user-review">
+                    <h3>Martin Nichols</h3>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p>I love these shoes so much. Since we have been in quarantine, the one thing that has kept me busy is just going for a walk. And these shoes are perfect for that. They are very comfortable and supportive. I also get many compliments on them. I would DEFINITELY recommend.</p>
+                </div>
+            </li>
+        </ul>
+    </section>
+    <!--CUSTOMER REVIEWS END-->
+    <!--CONTACT START-->
+    <section id="contact" class="contact">
+        <form method="POST" action="https://formspree.io/f/xnqldeyv">
+            <div class="input-wrap">
+                <label for="name">Full Name</label>
+                <input type="text" name="name" id="name" placeholder="Full Name" required>
+            </div>
+            <div class="input-wrap">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="Email" required>
+            </div>
+            <div class="input-wrap">
+                <label for="message">Message</label>
+                <textarea name="message" id="message" placeholder="Message" minlength="20"></textarea>
+            </div>
+            <button type="submit">Send</button>
+        </form>
+        <div class="logo-wrap">
+            <img src="images/global/logoTextWhite.png" alt="logo">
+        </div>
+    </section>
+    <!--CONTACT END-->`
+    return homePageElements;
+}
+export default homePage;
